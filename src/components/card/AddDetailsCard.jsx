49c0e87532c54c869c4e2c010bd937503123b3f4
yashodharjadav1425 <yashodharjadav1425@gmail.com>
2026-02-21 13:01:17 +0530

    second commit

---
+++ src/components/card/AddDetailsCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const AddDetailsCard = () => {
+    return(
+        <div className="row">
+            <div>
+                <div className='card'>
+                    <div className="card-header">
+                        <h6>
+                            Add Details
+                        </h6>
+                    </div>
+
+                    <div className='card-body'>
+
+                        <div className="row pl-2 pr-2">
+                            <div className='col-xl-3 col-lg-3 col-sm-3 colmspadding'>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AddDetailsCard;
+
